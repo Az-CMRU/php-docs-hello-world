0d91f445d9acfc8f8dc91070a4a8aaedfc3df5d2
Az-CMRU <111476688+Az-CMRU@users.noreply.github.com>
2022-08-18 13:45:42 +0700

    LogIn365-1

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
 
-echo "Hello Azure!";
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://unpkg.com/@microsoft/mgt/dist/bundle/mgt-loader.js"></script>
+  </head>
+  <body>
+        <?php
+
+            echo "Hello Azure!";
+
+        ?>
+        <mgt-msal2-provider client-id="YOUR-CLIENT-ID"></mgt-msal2-provider>
+        <mgt-login></mgt-login>
+  </body>
+</html>
